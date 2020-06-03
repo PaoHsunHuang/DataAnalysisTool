d17d0fccf31948bb665e022b57907af2a7699cee
PaoHsunHuang <37963003+PaoHsunHuang@users.noreply.github.com>
2020-06-03 14:45:15 +0800

    update generator
    allow generator multiple column, now create .csv file instead of .txt

--- Generator.py
+++ Generator.py
@@ -1,17 +1,38 @@
 import os
 import random
-name = input("File Name: ")
+inputString = input("File Name (column): ")
 rangeL = 1
 rangeH = 10
 dataCount = 10
+inputtemp = inputString.split()
+name = inputtemp[0]
+name += ".csv"
+columnCount = 1;
 
-name += ".txt"
+#read the number of column if exist
+if len(inputtemp) == 2:
+    columnCount = int(inputtemp[1])
+
+
 f = open(name, "w+")
+#write column name default number counting
+for i in range(columnCount):
+    f.write(str(i))
+    if i != (columnCount - 1):
+        f.write(",")
+
+f.write("\n")
+#write data by random number
 for i in range(dataCount):
-    temp = random.randint(rangeL, rangeH)
-    f.write(str(temp) + "\n")
-
-
+    for j in range(columnCount):
+        temp = random.randint(rangeL, rangeH)
+        f.write(str(temp))
+        if j != (columnCount - 1):
+            f.write(",")
+    f.write("\n")
 f.close()
+
+
+
 print("finish")
 input()
